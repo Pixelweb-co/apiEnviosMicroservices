2eb675cd5f37c05f7f9dbfbcada619062532f7a0
Edwin Guevara <admin@pixelweb.com.co>
2023-03-11 09:14:26 -0500

    calificaciones springboot

---
+++ calificar_service/src/main/java/com/calificaciones/calificar/CalificarApplication.java
@@ -0,0 +1,13 @@
+package com.calificaciones.calificar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalificarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalificarApplication.class, args);
+	}
+
+}
